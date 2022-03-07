bc563cbc2b59c85483d147392c67378eb3b29b66
luo-maria <1606665051@qq.com>
2022-03-07 13:48:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yst/Activity/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.yst.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.yst.R;
+
+
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/example/yst/service/StudentService.java
@@ -0,0 +1,5 @@
+package com.example.yst.service;
+
+public class StudentService {
+
+}
